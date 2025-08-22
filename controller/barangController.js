1cdadfb65a67f66f69e310d9cb782a0c153b20b1
Noriza scorpion <scorpionnovriza@gmail.com>
2025-08-22 16:38:23 +0700

    menambahkan validasi zod

--- controller/barangController.js
+++ controller/barangController.js
@@ -1,4 +1,11 @@
 import Barang from "../model/barang.js";
+import { z } from "zod";
+
+const BarangSchema = z.object({
+    nama_barang: z.string().min(1, "Nama wajib diisi"),
+    stok: z.number().min(0, "Stok tidak boleh negatif").max(100, "Stok tidak boleh lebih dari 100"),
+    harga: z.number().min(1000, "Harga harus lebih besar dari 500"),
+});
 
 class BarangController {
 
@@ -31,13 +38,28 @@ class BarangController {
 
     async create(req, res) {
         try {
-            const barang = await Barang.create(req.body);
+            const result = BarangSchema.safeParse(req.body);
+
+            if (!result.success) {
+                const fieldErrors = z.flattenError(result.error).fieldErrors;
+
+                return res.status(422).json({
+                    message: "Validation error",
+                    errors: fieldErrors
+                });
+            }
+
+            const barang = await Barang.create(result.data);
+
             res.status(201).json({
                 message: "Barang created successfully",
                 data: barang
             });
         } catch (error) {
-            res.status(500).json({ message: "Error creating barang", error: error.message });
+            res.status(500).json({
+                message: "Error creating barang",
+                error: error.message
+            });
         }
     }
 
@@ -47,13 +69,27 @@ class BarangController {
             if (!barang) {
                 return res.status(404).json({ message: "Barang not found" });
             }
-            await barang.update(req.body);
+
+            const result = BarangSchema.safeParse(req.body);
+            if (!result.success) {
+                const fieldErrors = z.flattenError(result.error).fieldErrors;
+                return res.status(422).json({
+                    message: "Validation error",
+                    errors: fieldErrors
+                });
+            }
+
+            await Barang.update(result.data);
+
             res.status(200).json({
                 message: "Barang updated successfully",
                 data: barang
             });
         } catch (error) {
-            res.status(500).json({ message: "Error updating barang", error: error.message });
+            res.status(500).json({
+                message: "Error updating barang",
+                error: error.message
+            });
         }
     }
 
