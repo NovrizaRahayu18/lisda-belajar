d65cdf872e56a8236c1511620e8498fce14b2f8b
Lisda Wati Waruwu <lisdawatiwaruwu1485@gmail.com>
2025-08-23 10:40:30 +0700

    perbaiki nama path

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,14 @@
 import express from "express";
 import pegawaiController from "../controller/pegawaiController.js";
 import barangController from "../controller/barangController.js";
-import {getAll} from "../controller/pegawaiController.js";
+
 const router = express.Router();
 
-router.get("/pegawai", getAll);
+router.get("/data-pegawai", pegawaiController.getAll);
 router.get("/pegawai/:id", pegawaiController.getOne);
 router.post("/pegawai-create", pegawaiController.create);
-router.put("/pegawai/:id", pegawaiController.update);
-router.delete("/pegawai/:id", pegawaiController.delete);
+router.put("/pegawai-update/:id", pegawaiController.update);
+router.delete("/pegawai-delete/:id", pegawaiController.delete);
 
 router.get("/data-barang", barangController.getAll);
 router.get("/barang/:id", barangController.getOne);
